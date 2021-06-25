c8234c0a49172239f69b98ea90cb2b50c1b7a32e
Tahmid <you@example.com>
2021-06-25 20:37:08 +0600

    done!

--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
-
-from distutils.core import setup
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
 setup(
-  name = 'voix',         # How you named your package folder (MyLib)
-  packages = ['voix'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'It is a package that can generate voice for you',   # Give a short description about your library
-  author = 'Tahmid Ahnaf',                   # Type in your name
-  author_email = 'tahmidahnafxtra@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/voix',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/tahmidahnaf/voix/archive/refs/tags/0.1.tar.gz',    # I explain this later on
-  keywords = ['voix','voice','sound'],   # Keywords that define your package best
-  install_requires=[    
-        'gtts','os','googletrans' ,'playsound'
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+  name='voix',
+  version='0.1',
+  description='A tiny package that can generate voice for you',
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description_content_type="text/markdown",
+  author='Tahmid Ahnaf',
+  author_email='tahmidahnafxtra@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['voix' , 'voice' ,'sound' ,'speak' , 'speaker' , 'sound generator' , 'voice generator' , 'text to speech' ,'tts' , 'translate'], 
+  packages=find_packages(),
+  install_requires=['gtts', 'googletrans' , 'playsound'] 
 )
